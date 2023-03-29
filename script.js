cde78079f846a0188f2a38476278c07ec31a1f9c
bryan <bryanbanguera146@gmail.com>
2023-03-28 21:11:23 -0500

    funcional correo, validacion3

--- script.js
+++ script.js
@@ -19,33 +19,33 @@
 })(document);
 
 // contactForm
-((d) => {
-    const $form = d.querySelector(".contact-form"),
-    $loader = d.querySelector(".contact-form-loader"),
-    $response = d.querySelector(".contact-form-response");
+// ((d) => {
+//     const $form = d.querySelector(".contact-form"),
+//     $loader = d.querySelector(".contact-form-loader"),
+//     $response = d.querySelector(".contact-form-response");
 
-        $form.addEventListener("submit", (e)=> {
-        e.preventDefault();
-        $loader.classList.remove("none");
-        fetch("https://formsubmit.co/ajax/bryanbanguera146@gmail.com",{
-            method:"POST",
-            body: new FormData(e.target)
-        }).then((res) => (res.ok ? res.json():Promise.reject(res)))
-        .then(json => {
-            console.log(json);
-            $loader.classList.add("none");
-            location.hash = "#gracias";
-            $form.requestFullscreen();
-        })
-        .catch(err => {
-            console.log(err);
-            let message = err.statusText || "Ocurrio un Error al enviar, intentalo nuevamente"
-            $response.querySelector("h3").innerHTML = `Error${err.status}: ${message}`;
-            }).finally (() => {
-                $loader.classList.add("none");
-                setTimeout(() => {
-                    location.hash = "#close";
-                },3000);
-            });
-    });
-})(document);
+//         $form.addEventListener("submit", (e)=> {
+//         e.preventDefault();
+//         $loader.classList.remove("none");
+//         fetch("https://formsubmit.co/ajax/bryanbanguera146@gmail.com",{
+//             method:"POST",
+//             body: new FormData(e.target)
+//         }).then((res) => (res.ok ? res.json():Promise.reject(res)))
+//         .then(json => {
+//             console.log(json);
+//             $loader.classList.add("none");
+//             location.hash = "#gracias";
+//             $form.requestFullscreen();
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             let message = err.statusText || "Ocurrio un Error al enviar, intentalo nuevamente"
+//             $response.querySelector("h3").innerHTML = `Error${err.status}: ${message}`;
+//             }).finally (() => {
+//                 $loader.classList.add("none");
+//                 setTimeout(() => {
+//                     location.hash = "#close";
+//                 },3000);
+//             });
+//     });
+// })(document);
